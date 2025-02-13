b3a08d9df515fc68cb5375042970d21563bf4d1e
Adria Nykiel <adrian.nykiel@gmail.com>
2025-02-13 11:30:27 +0000

    vite build + tailwind css v4 import

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -10,7 +10,9 @@ const count = ref(0)
 
 <template>
   <h1>{{ msg }}</h1>
-
+  <h2 class="text-3xl font-bold underline">
+    Tailwind css import
+  </h2>
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
     <p>
